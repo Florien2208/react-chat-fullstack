a64d746686a4b3088a87aa1cacc92339c4553610
florien2208 <mahorompakanyiflorien@gmail.com>
2024-06-18 13:15:22 +0200

    update on chat

--- server/server.js
+++ server/server.js
@@ -29,7 +29,6 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
@@ -72,7 +71,6 @@ app.get("/chatHistory", async (req, res) => {
   }
 });
 
-
 app.post("/saveChatHistory", async (req, res) => {
   try {
     const { sender, recipient, messages } = req.body;
@@ -89,7 +87,7 @@ app.post("/saveChatHistory", async (req, res) => {
       })),
     });
     await chatHistory.save();
-
+    console.log("saved chat", chatHistory);
     res.status(200).send({ message: "Chat history saved successfully" });
   } catch (error) {
     console.error("Error saving chat history:", error);
@@ -192,7 +190,6 @@ io.on("connection", (socket) => {
     }
   });
 });
-
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
